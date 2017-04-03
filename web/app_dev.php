a043fe031f2e12c393e484fb96954a1f9821c896
Nairi Abgaryan <nairi.abgaryan@gmail.com>
2017-04-03 13:47:32 +0400

    commit

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+
 //use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
@@ -9,12 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
 //umask(0000);
 date_default_timezone_set('Asia/Tbilisi');
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require __DIR__ . '/../app/autoload.php';
 //Debug::enable();
 
 $kernel = new AppKernel('dev', true);
+
 //$kernel->loadClassCache();
 $request = Request::createFromGlobals();
+
 $response = $kernel->handle($request);
+
 $response->send();
 $kernel->terminate($request, $response);
